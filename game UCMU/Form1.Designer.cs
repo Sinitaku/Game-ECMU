a5f82d12081e8d5632a1588a3bc848d958ec1fd7
Sini Takkinen <s3tasi02@students.osao.fi>
2023-11-23 14:10:51 +0200

    Alotettu ulkoasua

---
+++ game UCMU/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace game_UCMU
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblGameName = new System.Windows.Forms.Label();
+            this.lblInfoStrt = new System.Windows.Forms.Label();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblGameName
+            // 
+            this.lblGameName.AutoSize = true;
+            this.lblGameName.BackColor = System.Drawing.Color.Snow;
+            this.lblGameName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGameName.Location = new System.Drawing.Point(196, 29);
+            this.lblGameName.Name = "lblGameName";
+            this.lblGameName.Size = new System.Drawing.Size(415, 25);
+            this.lblGameName.TabIndex = 1;
+            this.lblGameName.Text = "Elemental Conquest: Monsters Unleashed";
+            this.lblGameName.Click += new System.EventHandler(this.lblGameName_Click);
+            // 
+            // lblInfoStrt
+            // 
+            this.lblInfoStrt.AutoSize = true;
+            this.lblInfoStrt.BackColor = System.Drawing.Color.Snow;
+            this.lblInfoStrt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfoStrt.Location = new System.Drawing.Point(36, 94);
+            this.lblInfoStrt.Name = "lblInfoStrt";
+            this.lblInfoStrt.Size = new System.Drawing.Size(742, 160);
+            this.lblInfoStrt.TabIndex = 2;
+            this.lblInfoStrt.Text = resources.GetString("lblInfoStrt.Text");
+            // 
+            // btnNext
+            // 
+            this.btnNext.BackColor = System.Drawing.Color.Snow;
+            this.btnNext.Location = new System.Drawing.Point(597, 362);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(150, 50);
+            this.btnNext.TabIndex = 3;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = false;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(1, 278);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(319, 172);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.lblInfoStrt);
+            this.Controls.Add(this.lblGameName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblGameName;
+        private System.Windows.Forms.Label lblInfoStrt;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ game UCMU/Form2.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace game_UCMU
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblGameName2 = new System.Windows.Forms.Label();
+            this.lblEldhart = new System.Windows.Forms.Label();
+            this.lblSeraphina = new System.Windows.Forms.Label();
+            this.lblThalia = new System.Windows.Forms.Label();
+            this.lblDorian = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblGameName2
+            // 
+            this.lblGameName2.AutoSize = true;
+            this.lblGameName2.BackColor = System.Drawing.Color.Snow;
+            this.lblGameName2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGameName2.Location = new System.Drawing.Point(145, 29);
+            this.lblGameName2.Name = "lblGameName2";
+            this.lblGameName2.Size = new System.Drawing.Size(528, 25);
+            this.lblGameName2.TabIndex = 0;
+            this.lblGameName2.Text = "Elemental Conquest: Monsters Unleashed, characters";
+            // 
+            // lblEldhart
+            // 
+            this.lblEldhart.AutoSize = true;
+            this.lblEldhart.BackColor = System.Drawing.Color.Snow;
+            this.lblEldhart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEldhart.Location = new System.Drawing.Point(30, 101);
+            this.lblEldhart.Name = "lblEldhart";
+            this.lblEldhart.Size = new System.Drawing.Size(739, 60);
+            this.lblEldhart.TabIndex = 1;
+            this.lblEldhart.Text = "Eldhart\r\nEldhart is a young blond male and he loves swimming. He is a bit wild bu" +
+    "t is always ready to fight against \r\nthe monsters. His power is water. \r\n";
+            // 
+            // lblSeraphina
+            // 
+            this.lblSeraphina.AutoSize = true;
+            this.lblSeraphina.BackColor = System.Drawing.Color.Snow;
+            this.lblSeraphina.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSeraphina.Location = new System.Drawing.Point(30, 173);
+            this.lblSeraphina.Name = "lblSeraphina";
+            this.lblSeraphina.Size = new System.Drawing.Size(739, 60);
+            this.lblSeraphina.TabIndex = 2;
+            this.lblSeraphina.Text = "Seraphina\r\nSeraphina is brown curly haired female with expolsive attitude. She lo" +
+    "ves baking and having a party.         \r\nHer power is fire.";
+            // 
+            // lblThalia
+            // 
+            this.lblThalia.AutoSize = true;
+            this.lblThalia.BackColor = System.Drawing.Color.Snow;
+            this.lblThalia.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblThalia.Location = new System.Drawing.Point(31, 245);
+            this.lblThalia.Name = "lblThalia";
+            this.lblThalia.Size = new System.Drawing.Size(738, 60);
+            this.lblThalia.TabIndex = 3;
+            this.lblThalia.Text = "Thalia\r\nThalia is brown haired young female who enjoys being hours alone. She is " +
+    "a bit mysterious and no one    \r\nreally knows about her past. Her power is elect" +
+    "ricity.";
+            // 
+            // lblDorian
+            // 
+            this.lblDorian.AutoSize = true;
+            this.lblDorian.BackColor = System.Drawing.Color.Snow;
+            this.lblDorian.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDorian.Location = new System.Drawing.Point(32, 324);
+            this.lblDorian.Name = "lblDorian";
+            this.lblDorian.Size = new System.Drawing.Size(737, 60);
+            this.lblDorian.TabIndex = 4;
+            this.lblDorian.Text = "Dorian\r\nDorian is shy blackhaired male. He willprotect his friends without hesita" +
+    "tion and usually he doesn\'t like     \r\nbeing the hero. His power is earth.";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblDorian);
+            this.Controls.Add(this.lblThalia);
+            this.Controls.Add(this.lblSeraphina);
+            this.Controls.Add(this.lblEldhart);
+            this.Controls.Add(this.lblGameName2);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblGameName2;
+        private System.Windows.Forms.Label lblEldhart;
+        private System.Windows.Forms.Label lblSeraphina;
+        private System.Windows.Forms.Label lblThalia;
+        private System.Windows.Forms.Label lblDorian;
+    }
+}
